5ee41e5d416b95cced48ae6d808fff4954b0ac71
Houssem17 <houssem.benaicha@esprit.tn>
2021-01-18 11:21:03 +0100

    List

--- app/src/main/java/com/example/myapplicationb/MainActivity.java
+++ app/src/main/java/com/example/myapplicationb/MainActivity.java
@@ -1,22 +1,40 @@
 package com.example.myapplicationb;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private MyBroadcastReceiver MyReceiver;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listView = findViewById(R.id.listView);
+        String mTitle[] = {"com.google.android.gm"};
+        String mDescription[] = {"Gmail"};
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription);
+        listView.setAdapter(adapter);
+
+
         // Receive broadcast from External App
         MyReceiver = new MyBroadcastReceiver();
         IntentFilter intentFilter = new IntentFilter("com.example.myapplicationa.NOTIFICATION_LISTENER_EXAMPLE");
@@ -24,8 +42,49 @@ public class MainActivity extends AppCompatActivity {
         if(intentFilter != null)
         {
             registerReceiver(MyReceiver, intentFilter);
+
         }
     }
+
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+
+
+        MyAdapter (Context c, String title[], String description[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+
+
+
+            return row;
+        }
+    }
+
+
+
 
     @Override
     protected void onDestroy() {
--- app/src/main/java/com/example/myapplicationb/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplicationb/MyBroadcastReceiver.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class MyBroadcastReceiver extends BroadcastReceiver
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBroadcastReceiver extends BroadcastReceiver{
+  //  List<String[]> Packages =new ArrayList<String[]>();
+
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        String pack = intent.getStringExtra("package");
+
+       ArrayList<String> pack = intent.getStringArrayListExtra("package");
         Log.i("BR" ,"Data received:  " + pack);
     }
 }
